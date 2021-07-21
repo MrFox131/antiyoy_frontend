95a34e58bbfe04614ff7a2f49792ad6743fc7bc8
Bortsov Roman <r.bortsov@yandex.ru>
2021-07-21 16:47:05 +0300

    Сделал матрицу смежности городов

---
+++ src/cityMap.js
@@ -0,0 +1,522 @@
+const object = {
+	1: [
+		{
+			id: 4,
+			method: "walk"
+		},
+		{
+			id: 19,
+			method: "sea"
+		},
+		{
+			id: 22,
+			method: "sea"
+		}
+	],
+	2: [
+		{
+			id: 22,
+			method: "sea"
+		},
+		{
+			id: 35,
+			method: "sea"
+		}
+	],
+	3: [
+		{
+			id: 25,
+			method: "sea"
+		},
+		{
+			id: 36,
+			method: "sea"
+		}
+	],
+	4: [
+		{
+			id: 1,
+			method: "walk"
+		},
+		{
+			id: 5,
+			method: "walk"
+		}
+	],
+	5: [
+		{
+			id: 22,
+			method: "train"
+		},
+		{
+			id: 23,
+			method: "train"
+		}
+	],
+	6: [
+		{
+			id: 7,
+			method: "train"
+		},
+		{
+			id: 22,
+			method: "train"
+		},
+		{
+			id: 23,
+			method: "train"
+		}
+	],
+	7: [
+		{
+			id: 6,
+			method: "train"
+		},
+		{
+			id: 8,
+			method: "sea",
+		},
+		{
+			id: 22,
+			method: "sea"
+		},
+		{
+			id: 24,
+			method: "walk"
+		},
+		{
+			id: 25,
+			method: "sea"
+		}
+	],
+	8: [
+		{
+			id: 7,
+			method: "sea" 
+		},
+		{
+			id: 10,
+			method: "sea"
+		},
+		{
+			id: 23,
+			method: "sea"
+		},
+		{
+			id: 25,
+			method: "sea"
+		}
+	],
+	9: [
+		{
+			id: 23,
+			method: "sea"
+		}
+	],
+	10: [
+		{
+			id: 8,
+			method: "sea"
+		},
+		{
+			id: 15,
+			method: "sea"
+		},
+		{
+			id: 27,
+			method: "sea"
+		},
+		{
+			id: 29,
+			method: "walk"
+		}
+	],
+	11: [
+		{
+			id: 15,
+			method: "sea"
+		},
+		{
+			id: 25,
+			method: "sea"
+		}
+	],
+	12: [
+		{
+			id: 25,
+			method: "sea"
+		},
+		{
+			id: 31,
+			method: "sea"
+		}
+	],
+	13: [
+		{
+			id: 26,
+			method: "sea"
+		}
+	],
+	14: [
+		{
+			id: 16,
+			method: "train"
+		},
+		{
+			id: 27,
+			method: "train"
+		}
+	],
+	15: [
+		{
+			id: 11,
+			method: "sea"
+		},
+		{
+			id: 18,
+			method: "sea"
+		},
+		{
+			id: 30,
+			method: "walk"
+		}
+	],
+	16: [
+		{
+			id: 14,
+			method: "train"
+		},
+		{
+			id: 28,
+			method: "train"
+		},
+		{
+			id: 32,
+			method: "train"
+		}
+	],
+	17: [
+		{
+			id: 15,
+			method: "sea"
+		},
+		{
+			id: 28,
+			method: "train"
+		},
+		{
+			id: 29,
+			method: "sea"
+		},
+		{
+			id: 33,
+			method: "walk"
+		},
+		{
+			id: 34,
+			method: "train"
+		}
+	],
+	18: [
+		{
+			id: 15,
+			method: "sea"
+		},
+		{
+			id: 36,
+			method: "sea"
+		}
+	],
+	19: [
+		{
+			id: 32,
+			method: "train"
+		},
+		{
+			id: 34,
+			method: "train"
+		},
+		{
+			id: 35,
+			method: "sea"
+		}
+	],
+	20: [
+		{
+			id: 17,
+			method: "sea"
+		},
+		{
+			id: 34,
+			method: "sea"
+		},
+		{
+			id: 35,
+			method: "sea"
+		},
+		{
+			id: 36,
+			method: "sea"
+		}
+	],
+	21: [
+		{
+			id: 36,
+			method: "walk"
+		}
+	],
+	22: [
+		{
+			id: 1,
+			method: "sea"
+		},
+		{
+			id: 2,
+			method: "sea"
+		},
+		{
+			id: 5,
+			method: "train"
+		},
+		{
+			id: 6,
+			method: "train"
+		},
+		{
+			id: 7,
+			method: "sea"
+		}
+	],
+	23: [
+		{
+			id: 5,
+			method: "train"
+		},
+		{
+			id: 6,
+			method: "train"
+		},
+		{
+			id: 8,
+			method: "sea"
+		},
+		{
+			id: 9,
+			method: "sea"
+		},
+		{
+			id: 26,
+			method: "sea"
+		}
+	],
+	24: [
+		{
+			id: 7,
+			method: "wakl"
+		},
+		{
+			id: 25,
+			method: "walk"
+		}
+	],
+	25: [
+		{
+			id: 3,
+			method: "sea"
+		},
+		{
+			id: 7,
+			method: "sea"
+		},
+		{
+			id: 8,
+			method: "sea"
+		},
+		{
+			id: 11,
+			method: "sea"
+		},
+		{
+			id: 12,
+			method: "sea"
+		},
+		{
+			id: 24,
+			method: "walk"
+		}
+	],
+	26: [
+		{
+			id: 13,
+			method: "sea"
+		},
+		{
+			id: 23,
+			method: "sea"
+		},
+		{
+			id: 27,
+			method: "sea"
+		}
+	],
+	27: [
+		{
+			id: 10,
+			method: "sea"
+		},
+		{
+			id: 14,
+			method: "train"
+		},
+		{
+			id: 26,
+			method: "sea"
+		},
+		{
+			id: 28,
+			method: "train"
+		},
+		{
+			id: 29,
+			method: "sea"
+		}
+	],
+	28: [
+		{
+			id: 16,
+			method: "train"
+		},
+		{
+			id: 17,
+			method: "train"
+		},
+		{
+			id: 27,
+			method: "train"
+		},
+		{
+			id: 29,
+			method: "train"
+		}
+	],
+	29: [
+		{
+			id: 10,
+			method: "walk"
+		},
+		{
+			id: 27,
+			method: "sea"
+		},
+		{
+			id: 28,
+			method: "train"
+		},
+		{
+			id: 30,
+			method: "walk"
+		}
+	],
+	30: [
+		{
+			id: 15,
+			method: "walk"
+		},
+		{
+			id: 29,
+			method: "walk"
+		}
+	],
+	31: [
+		{
+			id: 12,
+			method: "sea"
+		},
+		{
+			id: 36,
+			method: "sea"
+		}
+	],
+	32: [
+		{
+			id: 16,
+			method: "train"
+		},
+		{
+			id: 19,
+			method: "train"
+		}
+	],
+	33: [
+		{
+			id: 17,
+			method: "walk"
+		}, 
+		{
+			id: 34,
+			method: "walk"
+		}
+	],
+	34: [
+		{
+			id: 17,
+			method: "train"
+		},
+		{
+			id: 19,
+			method: "train"
+		},
+		{
+			id: 20,
+			method: "sea"
+		},
+		{
+			id: 33,
+			method: "walk"
+		},
+		{
+			id: 35,
+			method: "sea"
+		}
+	],
+	35: [
+		{
+			id: 2,
+			method: "sea"
+		},
+		{
+			id: 19,
+			method: "sea"
+		},
+		{
+			id: 20,
+			method: "sea"
+		},
+		{
+			id: 34,
+			method: "sea"
+		}
+	],
+	36: [
+		{
+			id: 3,
+			method: "sea"
+		},
+		{
+			id: 12,
+			method: "sea"
+		},
+		{
+			id: 18,
+			method: "sea"
+		},
+		{
+			id: 20,
+			method: "sea"
+		},
+		{
+			id: 21,
+			method: "walk"
+		}
+	]
+}
