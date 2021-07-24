907c98dc09d429d30401fc36a0b9b9e4d9f99801
MrFox131 <mrfox131@MrFox.MrFox>
2021-07-24 20:13:49 +1000

    Some worakarounds on scale

---
+++ src/test/Card.js
@@ -0,0 +1,10 @@
+export class Card {
+    constructor(title, type, description, imageFront) {
+        this.title = title;
+        this.type = type;
+        this.description = description;
+        this.imageFront = imageFront;
+    }
+
+
+}
